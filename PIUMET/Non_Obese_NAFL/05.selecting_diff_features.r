0307230d322d29238c721a882c3e9ac962ecb65d
ddasic123 <74629994+ddasic123@users.noreply.github.com>
2021-11-03 19:02:34 +0900

    Update 05.selecting_diff_features.r

--- PIUMET/Non_Obese_NAFL/05.selecting_diff_features.r
+++ PIUMET/Non_Obese_NAFL/05.selecting_diff_features.r
@@ -3,8 +3,17 @@ library(data.table);library(plyr);library(igraph)
 setwd("d:/data/NAFLD/")
 
 #
-load("diff_features_non_obese_nash.rdata")
-dat1 = fread("lipid_non_obese_nafl.txt")
+metabolite = "lipid_bile"
+disease = "non_obese_nafl"
+
+#checked!!!
+dat1 = read.table("SNU_DEG_nafld.txt")
+gene_up = rownames(dat1)[dat1$logFC > 0 & dat1$adj.P.Val < 0.05]
+gene_do = rownames(dat1)[dat1$logFC < 0 & dat1$adj.P.Val < 0.05]
+
+#
+storage = paste0(metabolite, "_", disease, ".txt")
+dat1 = fread(storage)
 dat1 = data.frame(dat1)
 
 #
@@ -13,4 +22,5 @@ idx2 = which(dat1$p < 0.2 & dat1$fc < 1)
 lipid_up = dat1$SNU_ID[idx1]
 lipid_do = dat1$SNU_ID[idx2]
 
-save(file = "diff_features_lipid_non_obese_nafl.rdata", gene_up, gene_do, lipid_up, lipid_do)
+storage = paste0("diff_features_", metabolite, "_",disease, ".rdata")
+save(file = storage, gene_up, gene_do, lipid_up, lipid_do)
