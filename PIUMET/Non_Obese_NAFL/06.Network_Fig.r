e26e02d4cb5f3a92e7fa24fffde255db10c009c3
ddasic123 <74629994+ddasic123@users.noreply.github.com>
2021-11-10 11:11:49 +0900

    Update 06.Network_Fig.r

--- PIUMET/Non_Obese_NAFL/06.Network_Fig.r
+++ PIUMET/Non_Obese_NAFL/06.Network_Fig.r
@@ -4,7 +4,7 @@ setwd("d:/data/NAFLD/")
 
 #
 metabolite = "lipid_bile"
-disease = "non_obese_nafl"
+disease = "obese_nash"
 
 #
 storage = paste0(metabolite, "_", disease, "_DEG_HUB.rdata")
@@ -83,7 +83,7 @@ for(i in 1:length(candidate.gene)){
   
   #
   set.seed(2)
-  storage = paste0("./", metabolite, "_", disease, "_gene/", gene,"_", i, "_", metabolite, "_", disease, "_nafl.pdf")
+  storage = paste0("./", metabolite, "_", disease, "_gene/", gene,"_", i, "_", metabolite, "_", disease, ".pdf")
   pdf(storage, width = size, height = size)
   plot(g,
        vertex.frame.color = NA,
@@ -94,70 +94,3 @@ for(i in 1:length(candidate.gene)){
   dev.off()
   
 }
-
-
-#
-df = as.matrix(res[, c(1, 2)])
-df = unique(df)
-g = graph.edgelist(df, directed = T)
-g = simplify(g)
-
-#node.shape
-V(g)$shape = "circle"
-
-#nodel.color
-V(g)$color = adjustcolor("black", alpha.f = .1)
-
-#node.size
-V(g)$size = 5
-
-#lable.size
-V(g)$label.cex = 0.3
-idx1 = which(V(g)$name %in% f_up)
-V(g)[idx1]$label.cex = 1
-idx1 = which(V(g)$name %in% f_down)
-V(g)[idx1]$label.cex = 1
-idx1 = which(V(g)$name %in% candidate.gene)
-V(g)[idx1]$label.cex = 2
-
-#label.color
-V(g)$label.color = "black"
-idx1 = which(V(g)$name %in% f_up)
-V(g)[idx1]$label.color = "red"
-idx1 = which(V(g)$name %in% f_down)
-V(g)[idx1]$label.color = "blue"
-
-#
-edge = get.edgelist(g)
-pair = paste0(edge[, 1], "_", edge[, 2])
-E(g)$width = 2
-idx1 = which(pair %in% sig.pair.up)
-E(g)[idx1]$width = 3
-idx1 = which(pair %in% sig.pair.down)
-E(g)[idx1]$width = 3
-
-#edge_color
-E(g)$color = adjustcolor("grey", alpha.f = 0.2)
-idx1 = which(pair %in% sig.pair.up)
-E(g)[idx1]$color = adjustcolor("red", alpha.f = 0.2)
-idx1 = which(pair %in% sig.pair.down)
-E(g)[idx1]$color = adjustcolor("blue", alpha.f = 0.2)
-
-if(nrow(edge) > 30){
-  size = 10
-} else  if(nrow(edge) > 10){
-  size = 6
-} else{
-  size = 4
-}
-
-#
-set.seed(2)
-storage = paste0("network_non_obese_nash.pdf")
-pdf(storage, width = 30, height = 30)
-plot(g,
-     vertex.frame.color = NA,
-     vertex.label.degree = 0,
-     edge.arrow.size = 0
-)
-dev.off()
